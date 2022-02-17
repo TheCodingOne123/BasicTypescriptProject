f600f3fa3694eff3bae0c086dcc1e71d48051f37
Johannes Schmitt <Johannesschmitt92@gmail.com>
2022-02-17 12:28:28 +0100

    BasicWorkingExample

--- source/index.ts
+++ source/index.ts
@@ -1,17 +1,17 @@
-import {MongoClient} from "mongodb";
-
+import {database} from "./database";
 
 const express = require("express")
 const app = express()
 
 app.get('/Team',async (req, res) => {
 
-    let database = new MongoClient("mongodb://127.0.0.1:27017/")
+
     await database.connect()
+
     let databaseNames = await database.db().admin().listDatabases()
 
     res.send(databaseNames)
-
+    await database.close()
 
 })
 
